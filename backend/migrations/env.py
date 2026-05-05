2576d8542f4c67aaf230395e9dbe8c062232c417
Nguyen Hoang Nam Khanh <2212391@dlu.edu.vn>
2026-05-05 21:21:38 +0700

    feat:tao du lieu tu dong

--- backend/migrations/env.py
+++ backend/migrations/env.py
@@ -5,9 +5,7 @@ from sqlalchemy import pool
 
 from alembic import context
 
-from models import Base  # Import Base từ file models.py của bạn
-
-target_metadata = Base.metadata  # Thiết lập metadata để Alembic theo dõi
+from app.models import Base  # Import Base từ file models.py của bạn
 
 # this is the Alembic Config object, which provides
 # access to the values within the .ini file in use.
@@ -22,7 +20,7 @@ if config.config_file_name is not None:
 # for 'autogenerate' support
 # from myapp import mymodel
 # target_metadata = mymodel.Base.metadata
-target_metadata = None
+target_metadata = Base.metadata # Thiết lập metadata để Alembic theo dõi
 
 # other values from the config, defined by the needs of env.py,
 # can be acquired:
